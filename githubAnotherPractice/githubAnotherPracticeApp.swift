657c5209c275b2316f077700ae389b94cf05e6a6
Paige <makipaige11@gmail.com>
2023-07-18 09:25:56 -0500

    Initial Commit

---
+++ githubAnotherPractice/githubAnotherPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubAnotherPracticeApp.swift
+//  githubAnotherPractice
+//
+//  Created by Paige Maki on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct githubAnotherPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
